ce3979b91bb185dafa9c0355ca81196e278353b0
RstTiwari <iamrrt2121@gmail.com>
2023-05-13 01:06:52 +0530

    added day2

--- day2.js
+++ day2.js
@@ -2,18 +2,40 @@
  * Q What are the  javascript Accessors;
  */
 
+const { log } = require("console");
+
 let user = {
-    name:'Rohit Kumar',
-    area:"Vasai VIlage",
-    sub:["math", "hind"],
+  name: "Rohit Kumar",
+  area: "Vasai VIlage",
+  sub: ["math", "hind"],
 
-    get subject (){
-        return this.sub
-    },
+  get subject() {
+    return this.sub;
+  },
 
-    set subject(value){
-           return this.sub.push(value)
-    }
-}
+  set subject(value) {
+    let investment = value.amount + value.subTotal;
+    return (this.amount = investment);
+  },
+};
 
-console.log(user.subject) //["math"]
+user.subject = { amount: 100, month: 2, subTotal: 500 };
+console.log(user); // please run this code to see the outputr baby ;
+
+// Q if No break in Switch Case;
+let key = 1
+switch (key) {
+    case 0:
+        console.log("case 0");
+        break;
+    case 1:
+        console.log("case 1");
+    case 2:
+        console.log( "case 2");
+
+    default:
+        break;
+};
+
+
+
