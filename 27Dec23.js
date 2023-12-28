d61cd9cebb6b7e1ccced33b9a0ef94d8268e8585
RstTiwari <iamrrt2121@gmail.com>
2023-12-28 23:16:25 +0530

    added new Changes

--- 27Dec23.js
+++ 27Dec23.js
@@ -1,19 +1,46 @@
-const canPlaceFlower = (flowerBed, n) => {
+const canPlaceFlowers = (flowerBed, n) => {
     let count = 0;
-    for (let i = 0; i < flowerBed.length; i++) {
-        if (count === n) return true;
-        let left = flowerBed[i];
-        let center = flowerBed[i + 1];
-        let right = flowerBed[i + 2];
-        if (left !== center && center !== right) {
-            count += 1;
-        }
+
+    for(let i = 0; i < flowerBed.length; i++) {
+        if(
+            (flowerBed[i] === 0) && 
+            ( i === 0 || flowerBed[i-1] === 0) &&
+            (  i === flowerBed.length - 1 || flowerBed[i + 1] === 0 )
+            ) {
+                flowerBed[i] = 1;
+                count++;
+            } 
+
+        if(count >= n) return true;
     }
-    return false
-    
 
-    
-};
+    return false;
 
-console.log(canPlaceFlower([1,0,0,0,0,0,1], 2));
-// console.log(canPlaceFlower([0,1,0,0,0],1))
+}
+
+const canPlaceFlowers2 = (flowerBed, n) => {
+    let count = 0 , i = 0;
+     while (i < flowerBed.length){
+         if (
+             (flowerBed[i] === 0) &&
+             (i === 0 || flowerBed[i - 1] === 0) &&
+             (i === flowerBed.length - 1 || flowerBed[i + 1] === 0)
+         ) {
+             count += 1;
+             i += 2;
+         } else {
+             i += 1;
+         }
+         if(count >= n)  return true
+     }
+     
+
+    return false;
+
+}
+
+
+
+//console.log(canPlaceFlowers([1, 0, 0, 0, 0, 0, 1], 2)); 
+console.log(canPlaceFlowers2([0,0,1,0,0,0 ,0],1))
+console.log(canPlaceFlowers2([1,0,0,0,1,0,0],2))
