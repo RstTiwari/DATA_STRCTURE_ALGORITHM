1e958f16996ac731b46f3d76c3487c061120a066
RstTiwari <iamrrt2121@gmail.com>
2024-03-07 00:17:04 +0530

    Added Recusion Function check

--- 2024/recursion.js
+++ 2024/recursion.js
@@ -65,3 +65,76 @@ function productOfArray(array) {
     return array[0] + productOfArray(array.slice(1));
 }
 console.log(productOfArray([2, 3, 4, 5, 67, 8, 89]));
+
+
+{
+    // accept a number and give sum of number from 0 till that number
+}
+function recursiveRange(num){
+   if(num ===0 ){
+    return 0
+   }
+   const sum = num + recursiveRange(num - 1)
+   return sum
+}
+console.log(recursiveRange(10));
+
+
+
+
+function fib(num , a =0 , b = 1 ){
+      if(num <= 0){
+        return a
+      }
+      const nthNumber = fib(num -1 , b ,a +b)
+      return nthNumber
+  }
+
+console.log(fib(35))
+
+function reversStringRecusrsion (str){
+     if(str.length <= 1){
+        return str[0]
+     }
+
+     const reversed = reversStringRecusrsion(str.substring(1))
+     return reversed + str[0]
+}
+
+console.log(reversStringRecusrsion("rohit"))
+
+/**
+ * @params Wirte recursive fucntion to check if the given value is Palindrom or not
+ *  @input Takes a String 
+ * @output return binary value weather true or not
+ */
+
+function checkPalindrome (str){
+    console.log(str);
+    if(str.length <= 1){
+        return str[0]
+    }
+    // helper function two check if first and last valuers are equal or not
+    if(firstValue(str) !== lastValue(str)){
+        return false
+    }
+
+    // now Helper fumnction to get middle of String with first and last vlaue
+    checkPalindrome(getMidleString(str))
+    return true
+}
+console.log(checkPalindrome("tenet"))
+
+function firstValue (str){
+    console.log(str[0]);
+    return str[0]
+}
+function lastValue (str){
+    console.log(str[str.length -1]);
+    return str[str.length -1]
+}
+
+function getMidleString (str){
+    console.log(str[1,-1])
+    return str[1 ,-1]
+}
