79a6ae3f40740f9aec095615dd2b72d3096a5dca
Rst(Rohit) Tiwari <rohittiwari@hkbdevelopment.com>
2024-03-22 16:54:33 +0530

    Added Changes

---
+++ 2024/object/method.js
@@ -0,0 +1,8 @@
+/**
+ * Object.assign(target,source1,source2,source3......sourceN)
+ * @param target the target object wuchi will be going to be updated
+ * @param socurce // the objects which will add to the target object
+ * @returns target ==> target Object with updated Object to return
+ */
+
+
