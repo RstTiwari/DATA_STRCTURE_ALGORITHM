765fdcc7588f8c0805cb8ed91898de4a7d0b9bef
RstTiwari <iamrrt2121@gmail.com>
2024-03-28 23:58:08 +0530

    Feature array method

---
+++ 2024/array/method.js
@@ -0,0 +1,23 @@
+/**Learning all the best possible method of array class */
+
+/**
+ * Array.prototype.at(index)
+ * @param index of the value we gonna retirive from it
+ * @returns value at that index point
+ *
+ */
+
+const arrayat = [12, 23, 123, 1343, 1334];
+console.log(arrayat.at(6)); //undefined  ==> index  > array.length;
+console.log(arrayat.at(-6)); // undefined   ==> index < - array.length
+console.log(arrayat.at(-1)); // 1334
+
+/*******This method same as the method of concat strign in js just be  Revise that at anypoint of time */
+
+/**
+ * Array.prototype.concat(array1,array2 ,array3 ,array4 ,.....arrayN)
+ */
+
+/**
+ * Array.proptoype.
+ */
