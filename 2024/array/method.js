8617fa865c32780fc3533d9088dbd57e4e3d5018
RstTiwari <iamrrt2121@gmail.com>
2024-03-30 01:20:19 +0530

    Array method

--- 2024/array/method.js
+++ 2024/array/method.js
@@ -19,5 +19,47 @@ console.log(arrayat.at(-1)); // 1334
  */
 
 /**
- * Array.proptoype.
+ * Array.proptoype. copyWithin(target,start,end)
+ * @param target
+ * @param start
+ * @param end
+ */
+
+const copyWithinArray = [1, 2, 34, 5, 5];
+console.log(copyWithinArray.copyWithin(1, 3, 4)); //[ 1 ,5,34,5,5]
+console.log(copyWithinArray.copyWithin(1, 3)); //[ 1 ,5,5,5,5]
+console.log(copyWithinArray.copyWithin(3));
+
+const copyWithinarray2 = [2, 3, 45, 2, 4, 67, 3455];
+console.log(copyWithinarray2.copyWithin(2, 4)); // [1,3,23,4,67,3455,67,3445]
+console.log(copyWithinarray2.copyWithin(2)); // try to understand this much better way in Reivsion
+
+/**
+ * Array.prototype.entries(array)
+ * @param array the array which key object values need to be found
+ * @returns key obj values
+ */
+
+const entriesArray = [2, 3, 45, 4, "rohit"];
+console.log(entriesArray.entries()); // [Array Itreator]
+
+for (const entries of entriesArray.entries()) {
+    console.log(entries);
+}
+
+/**
+ * Array.prototype.every(callBackFn ,thisArg)
+ * @param callBackFn directly pass  a fucntion test the true and false scenrio on the array
+ * @returns true or false even on element fails it returns false
+ */
+
+function checkLessThan(element, index, array) {
+    console.log(element, index, array, "-----");
+    return element >= 1;
+}
+const everyArray = [2, 6, 9, 10, 11, 20];
+console.log(everyArray.every(checkLessThan), "---");
+
+/**
+ * Array.prototype.
  */
