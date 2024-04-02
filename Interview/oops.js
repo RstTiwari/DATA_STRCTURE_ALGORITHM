774d2c4587e80b00bf527c57fd7e02b9a5c7b130
Rst(Rohit) Tiwari <rohittiwari@hkbdevelopment.com>
2024-04-02 17:47:18 +0530

    Added Oops Feature

---
+++ Interview/oops.js
@@ -0,0 +1,61 @@
+/**UnderStading OOP in javascript in much details going deeper */
+// Eg want to design a school system  software now let usnderstand OOPS from this perspetction
+
+class Person {
+    constructor(name, sirName) {
+        (this.name = name), (this.sirName = sirName);
+    }
+
+    introduceSelf() {
+        console.log(`Hi i am  ${this.name}, how you doing`);
+    }
+}
+
+const rohit = new Person("Rohit", "tiwari"); // rohit in instance of class Person
+
+class Proffersor extends Person {
+    constructor(name, sirName, teaches) {
+        super(name, sirName);
+        this.teaches = teaches;
+    }
+    grade() {
+        console.log("the Grades for the Given sunare ", `${this.teaches}`);
+    }
+    introduceSelf() {
+        console.log(
+            `Hi i am Proffesor ${this.name}  i will be teaching you ${this.teaches}`
+        );
+    }
+}
+
+//math sir is instance class Proffesor
+// Preffesor inheriteds Parent class Proffesor values
+const mathSir = new Proffersor("MathValeSir", ["Math", "Science", "Art"]);
+
+class Student extends Person {
+    constructor(name, sirName, year) {
+        super(name, sirName);
+        this.year = year;
+    }
+    favrateSubject(subject) {
+        if (!subject) throw new Error("Please Provide Subject");
+        console.log(
+            `My favrate subject are invloing and solving the ${subject}`
+        );
+    }
+    introduceSelf() {
+        console.log(
+            `Hi my name is ${this.name}  and i m studnet of batch ${this.year}`
+        );
+    }
+}
+
+// studenOfSummeris instance of Class Student
+// it also intertetaes propetiy from Parent class Person
+const studenOfSummer = new Student("summer", "tiwari", 2024);
+
+// introduceSelf is ploytmoprphisem fucntion which behaved diffrenctly in diffent enviorment
+studenOfSummer.introduceSelf();
+mathSir.introduceSelf();
+
+/**The method int */
