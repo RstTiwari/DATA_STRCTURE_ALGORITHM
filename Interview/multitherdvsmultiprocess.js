fd7f8fde000239c88f5a7b04d0d08bfaad0a8f63
Rst(Rohit) Tiwari <rohittiwari@hkbdevelopment.com>
2024-05-23 14:08:39 +0530

    Practise for Single thread aand Multithread

---
+++ Interview/multitherdvsmultiprocess.js
@@ -0,0 +1,26 @@
+//Multiprcess vs multithereding
+//Multiprocess is also called as Parllism
+
+/*Before going deeper in Multiprcess and multitherding lets understand basics concept of
+
+Programme  => A program is a static set of instructions written in a programming language  ( but not like runinng in state)
+Process => A process is a dynamic instance of a program that is being executed. When you run a program, 
+Thread  =>  A basic unit of CPU utilization within the given Process. it has same memory and location as its parent process.
+*/
+
+/**Diffrent Stages Of Threading  **/
+//New Thread =>  a stage when a thred is created but not running yet nor excuting anykind of code yet .
+//Running State => The  running state is state wehre its running and excuting the task
+//Block or Waiting State = >  a thread whcih waiting to accquire resoureces or waiting for other task to complete
+//Terminate => when task completed naturally or normally or due to certain erros in system.
+
+
+
+/**
+ *MultiThreading || Concurrency  => A process where multiple
+*/
+
+
+/**
+*Muitiprocessing   
+*/
