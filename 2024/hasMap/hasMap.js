9f4ff4bc8f2eb81e0a6de5609e69d8911a1b02ad
RstTiwari <iamrrt2121@gmail.com>
2024-05-27 23:56:02 +0530

    Longest substring

--- 2024/hasMap/hasMap.js
+++ 2024/hasMap/hasMap.js
@@ -1,26 +1,24 @@
-function twoSum2(nums,target){
-    let hasMap = new Map()
-    for(let i = 0 ; i < nums.length ; i++){
-        let complement = target -nums[i]
-        if(hasMap.has(complement)){
+function twoSum2(nums, target) {
+    let hasMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (hasMap.has(complement)) {
             //return [hasMap.get(complement),i]
         }
-        hasMap.set(nums[i],i)
+        hasMap.set(nums[i], i);
     }
-
 }
 
-console.log(twoSum2([2,7,11,15],9));
+console.log(twoSum2([2, 7, 11, 15], 9));
 
 function longestSubString(string) {
     let hasMap = new Map();
     let current = "";
     let longest = "";
     for (let i = 0; i < string.length; i++) {
-        console.log(longest,current,hasMap,i);
         if (!hasMap.has(string[i])) {
             current += string[i];
-            hasMap[string[i]] = i;
+            hasMap.set(string[i], i);
         } else {
             if (current.length > longest.length) {
                 longest = current;
@@ -28,18 +26,18 @@ function longestSubString(string) {
             current = "";
             hasMap.clear();
         }
-    }
 
+        console.log(current, longest);
+    }
+    if (current.length > longest.length) {
+        longest = current;
+    }
+    console.log(longest, current);
     return longest.length;
 }
 
-
-console.log(longestSubString("abcabcbb"))
+// console.log(longestSubString("abcabcbb"));
+console.log(longestSubString("aab"));
 /**
- * 1                          i        hasMap = {a =>0} current = a
- * 2                           i       hasMap = {a=>0,b=>1} current = ab
- * 3                            i       hasMap = {a => 0 , b=> 1,c =>2 } current = abc
- * 4                             i      
+ * 1
  */
-
-
