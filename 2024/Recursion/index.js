25ba6aef260b8f87067957eca0f6156b4e89c9d2
Rst(Rohit) Tiwari <rohittiwari@hkbdevelopment.com>
2024-06-19 17:46:49 +0530

    Added this features

--- 2024/Recursion/index.js
+++ 2024/Recursion/index.js
@@ -1,6 +1,6 @@
 /**
- * 
- * @param {int} n  upto you want print 
+ *
+ * @param {int} n  upto you want print
  * @param {*} current   value to Fibonacci series going to start   default 0
  * @param {*} next      value to be next in fiance Series default = 1
  * @param {*} count     to keep track numbers of values presented in
@@ -13,17 +13,18 @@ function fibonacciSeries(n, current = 0, next = 1, count = 0) {
     }
 }
 
-
 //fibonacciSeries(20)
 
-const fibonacciSeriesVar = (n, current = 0, next = 1, count = 0)=>{
+const fibonacciSeriesVar = (n, current = 0, next = 1, count = 0) => {
     if (count < n) {
         console.log(current);
         fibonacciSeries(n, next, current + next, count + 1);
     }
-}
+};
 
-fibonacciSeriesVar(10)
-
-
+fibonacciSeriesVar(10);
 // Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+function validPalindrome() {
+    
+}
