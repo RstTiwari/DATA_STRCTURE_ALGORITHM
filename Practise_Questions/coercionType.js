e0e27de4f178d56d58dcf51299de4ff1f161b00b
Rst Tiwari <79466131+RstTiwari@users.noreply.github.com>
2024-06-28 21:30:50 +0530

    Update coercionType.js

--- Practise_Questions/coercionType.js
+++ Practise_Questions/coercionType.js
@@ -27,3 +27,26 @@ console.log(1-[]) // 1
 console.log(1*"2") //2
 console.log(8 /"4") // 2
 console.log(3%"3") // 
+
+
+
+
+console.log(1/null) // Infinity becasue null turns to be 0
+
+
+
+// Booolens values with all signs and opreator
+
+console.log(true +1) // 2
+console.log(false - 2)// -2
+console.log(true + undefined) // NaN
+console.log(true + []) // " 1"
+console.log(false - []) // "0"
+console.log(false - {}) //NaN
+console.log(true + {})// 1 Object
+console.log(true + []) // "1"
+
+
+//Coeerison in equlity check 
+console.log( [] ==[]) // false
+
