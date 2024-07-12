1bbf012a715d8c49d5fe6ffb1cb9e12e67799920
RstTiwari <iamrrt2121@gmail.com>
2024-07-12 10:14:24 +0530

    added Occurance from Backend

--- 2024/Multipointer/FirstOccuranceOfString.js
+++ 2024/Multipointer/FirstOccuranceOfString.js
@@ -22,7 +22,7 @@ var strStr = function (haystack, needle) {
     while (cur < haystack.length) {
         if (haystack[cur] !== needle[next]) {
             next = 0;
-            cur -next
+            cur - next;
         } else {
             next++;
 
@@ -35,5 +35,46 @@ var strStr = function (haystack, needle) {
     return -1;
 };
 
-console.log(strStr("mississippi", "issip"))
+console.log(strStr("mississippi", "issip"));
 
+function firstOccurrenceOfString(string, word) {
+    let cur = 0;
+    let next = 0;
+
+    while (cur < string.length) {
+        if (string[cur] !== word[next]) {
+            cur = cur - next + 1;
+            next = 0;
+        } else {
+            next++;
+            if (next === word.length) {
+                return cur - next + 1;
+            }
+            cur++;
+        }
+    }
+    return -1;
+}
+
+console.log(firstOccurrenceOfString("hello", "ll"));
+console.log(firstOccurrenceOfString("mississippi", "issip"));
+
+function lastOccuranceOfString(string, word) {
+    let last = string.length - 1;
+    let pre = word.length - 1;
+    while (last >= 0) {
+        if (string[last] !== word[pre]) {
+            last += word.length - pre - 1;
+            pre = word.length - 1;
+        } else {
+            if (pre === 0) {
+                return last;
+            }
+            pre--;
+        }
+        last--;
+    }
+    return -1;
+}
+
+console.log(lastOccuranceOfString("hllello", "ll"));
