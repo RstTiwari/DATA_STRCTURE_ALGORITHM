b511d7284c9a2d78bdceda7c68654a59168428c3
RstTiwari <iamrrt2121@gmail.com>
2024-09-03 23:13:26 +0530

    added Mrgin two sroted arrays

--- 2024/BinarySearch/BinarySearch.js
+++ 2024/BinarySearch/BinarySearch.js
@@ -25,4 +25,47 @@ console.log(bianarySearch([1, 2, 3, 4, 5, 8, 9], 5));
 //  [1, 2, 3, 4, 5, 8, 9]
 //   s     e
 
+function medianOfTwoSortedArray(num1, num2) {
+    let sortedArray = [];
+    let left = 0;
+    let right = 0;
+    while (left < num1.length && right < num2.length) {
+        if (num1[left] <= num2[right]) {
+            sortedArray.push(num1[left]);
+            left++;
+        } else {
+            sortedArray.push(num2[right]);
+            right++;
+        }
+    }
 
+    // if there is remainig element in num 1 add them to sortedArray
+    while (left < num1.length) {
+        sortedArray.push(num1[left]);
+        left++;
+    }
+
+    // if there is anything remaning in the num2 add it to sortedArray
+
+    while (right < num2.length) {
+        sortedArray.push(num2[right]);
+        right++;
+    }
+
+    let median;
+    let n = sortedArray.length;
+    if (n % 2 === 0) {
+        median =
+            (sortedArray[Math.floor((n - 1) / 2)] +
+                sortedArray[Math.floor(n / 2)]) /
+            2;
+    } else {
+        median = sortedArray[Math.floor(n / 2)];
+    }
+    console.log(sortedArray);
+    return median;
+}
+
+console.log(medianOfTwoSortedArray([1, 2, 5, 15, 18], [2, 3, 6, 9, 10]));
+// take the the second num put it first nums by binary Search logic
+// then take median of the first array
