d0094bf88283b934224d15f921bf4fba705b2926
RstTiwari <iamrrt2121@gmail.com>
2024-09-26 22:19:39 +0530

    added selection sort

---
+++ DSA_ALOGO/InsersationSort/insertationsoirt.js
@@ -0,0 +1,15 @@
+function insersationSort(array) {
+    let i = 0;
+    let start = 1;
+
+    while (i < array.length) {
+        if (array[i] < array[i - 1]) {
+            let j = 0;
+            while (i === j) {}
+        } else {
+        }
+        i++;
+    }
+}
+
+let array = [3, 2, 6, 4, 8, 4, 89, 4];
