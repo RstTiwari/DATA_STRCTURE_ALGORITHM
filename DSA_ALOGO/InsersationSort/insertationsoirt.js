eabfca20e4c3e44f3fdd4a4da8f34f6f8c6f239d
Rst(Rohit) Tiwari <rohittiwari@hkbdevelopment.com>
2024-09-27 11:05:53 +0530

    Bubble sort ,selection sort and Insertion sort in details

--- DSA_ALOGO/InsersationSort/insertationsoirt.js
+++ DSA_ALOGO/InsersationSort/insertationsoirt.js
@@ -1,15 +1,73 @@
-function insersationSort(array) {
-    let i = 0;
-    let start = 1;
+let array = [90, 3, 2, 6, 4, 8, 4, 89, 4];
 
+function insertionSort(array) {
+    let i = 1;
     while (i < array.length) {
-        if (array[i] < array[i - 1]) {
-            let j = 0;
-            while (i === j) {}
-        } else {
+        let key = array[i];
+        let j = i - 1;
+
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            j--;
         }
+
+        array[j + 1] = key;
         i++;
     }
+    return array;
 }
 
-let array = [3, 2, 6, 4, 8, 4, 89, 4];
+console.log(insertionSort(array));
+
+function bubbleSort(array) {
+    let i = array.length - 1;
+    while (i >= 0) {
+        let j = 0;
+        while (j < i) {
+            if (array[j] > array[i]) {
+                [array[j], array[i]] = [array[i], array[j]];
+            }
+            j++;
+        }
+        i--;
+    }
+    return array;
+}
+
+function selectionSort(array) {
+    let min = 0;
+    let i = 0;
+    while (i < array.length) {
+        min = i;
+        let j = i + 1;
+        while (j < i) {
+            if (array[j] < array[min]) {
+                min = j;
+            }
+            j++;
+        }
+        [array[i], array[min]] = [array[min], array[i]];
+        i++;
+    }
+    return array;
+}
+console.log(bubbleSort(array));
+console.log(selectionSort(array));
+
+let array2 = [90, 3, 2, 6, 4, 8, 4, 89, 4];
+function insertionSort2(array) {
+    let i = 1;
+    while (i < array.length) {
+        let current = array[i];
+        let j = i - 1;
+        while (j >= 0 && array[j] > current) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = current;
+        i++;
+    }
+    return array;
+}
+
+console.log(insertionSort2(array2));
