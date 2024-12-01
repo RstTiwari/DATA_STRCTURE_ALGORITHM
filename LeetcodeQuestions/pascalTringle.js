ac8ef7a16ad91df714a7e8f21a627d8f6784f72d
RstTiwari <iamrrt2121@gmail.com>
2024-12-01 19:09:15 +0530

    added pascal Tringle question

--- LeetcodeQuestions/pascalTringle.js
+++ LeetcodeQuestions/pascalTringle.js
@@ -5,7 +5,7 @@ function pasCalVariationOne(row, col) {
   return element;
 }
 
-function nCr(n, r){
+function nCr(n, r) {
   let res = 1;
   for (let i = 0; i < r; i++) {
     res = res * (n - i);
@@ -13,8 +13,62 @@ function nCr(n, r){
   }
   return res;
 }
-console.log(pasCalVariationOne(6, 3));
+//console.log(pasCalVariationOne(5, 3));
 
-function pasCalVariationTwo(){
-
+// finding the entire nth row
+function pasCalVariationTwo(n) {
+  let row = "";
+  for (let c = 1; c < n; c++) {
+    console.log(nCr(n - 1, c - 1), "=");
+  }
+  return row;
 }
+
+//console.log(pasCalVariationTwo(5));
+
+function pasCalVariationThreeNaive(rowNumber) {
+  let result = [];
+
+  for (let row = 1; row <= rowNumber; row++) {
+    let temArray = [];
+    for (let col = 1; col <= row; col++) {
+      let ele = nCr2(row - 1, col - 1);
+      temArray.push(ele);
+    }
+    result.push(temArray);
+  }
+
+  return result;
+}
+
+function nCr2(n, r) {
+  let res = 1;
+  for (let row = 0; row < r; row++) {
+    res = res * (n - row);
+    res = res / (row + 1);
+  }
+  return res;
+}
+
+console.log(pasCalVariationThreeNaive(0));
+
+function pasCalVariationThreeOptimal(totalRow) {
+  let res = [];
+  for (let row = 1; row <= totalRow; row++) {
+    res.push(genrateRow(1));
+  }
+  return res;
+}
+
+function genrateRow(row) {
+  let ans = 1;
+  let res = [1]; // put the first vale
+  for (let col = 1; col < row; col++) {
+    ans = ans * (row - col);
+    ans = ans / col;
+    res.push(ans);
+  }
+  console.log(res, "==");
+  return res;
+}
+console.log(pasCalVariationThreeOptimal(1));
