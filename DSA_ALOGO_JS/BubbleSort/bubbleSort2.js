990df19e4dafb1f7c9c29eda09ba823bf743d6ec
RstTiwari <iamrrt2121@gmail.com>
2024-12-07 16:50:21 +0530

    practise

--- DSA_ALOGO_JS/BubbleSort/bubbleSort2.js
+++ DSA_ALOGO_JS/BubbleSort/bubbleSort2.js
@@ -1,27 +1,26 @@
 function bubbleSort(array) {
-    let i = array.length - 1;
-    while (i >= 0) {
-        let j = 0;
-        console.log(i);
-        while (j < i) {
-            if (
-                array[j].grade > array[i].grade ||
-                (array[j].grade == array[i].grade &&
-                    array[j].name > array[i].name)
-            ) {
-                [array[j], array[i]] = [array[i], array[j]];
-            }
-            j++;
-        }
-        i--;
+  let i = array.length - 1;
+  while (i >= 0) {
+    let j = 0;
+    console.log(i);
+    while (j < i) {
+      if (
+        array[j].grade > array[i].grade ||
+        (array[j].grade == array[i].grade && array[j].name > array[i].name)
+      ) {
+        [array[j], array[i]] = [array[i], array[j]];
+      }
+      j++;
     }
-    return array;
+    i--;
+  }
+  return array;
 }
 
 const students = [
-    { name: "John", grade: 85 },
-    { name: "Jane", grade: 90 },
-    { name: "Emma", grade: 90 },
-    { name: "Alice", grade: 85 },
+  { name: "John", grade: 85 },
+  { name: "Jane", grade: 90 },
+  { name: "Emma", grade: 90 },
+  { name: "Alice", grade: 85 },
 ];
 console.log(bubbleSort(students), "===re");
