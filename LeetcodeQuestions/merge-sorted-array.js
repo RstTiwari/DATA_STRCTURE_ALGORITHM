64901d8cdf8434873b9da96f563463c277b27ad3
RstTiwari <iamrrt2121@gmail.com>
2024-12-19 23:02:18 +0530

    finding the missing number

--- LeetcodeQuestions/merge-sorted-array.js
+++ LeetcodeQuestions/merge-sorted-array.js
@@ -14,7 +14,7 @@ function optimal1(num1, m, num2, n) {
   return [...num1, ...num2];
 }
 
-console.log(optimal1([1, 2, 3], 3, [2, 5, 6], 3));
+//console.log(optimal1([1, 2, 3], 3, [2, 5, 6], 3));
 // will not work on leet code because of other empty spots
 
 function optimal2(num1, m, num2, n) {
@@ -25,20 +25,60 @@ function optimal2(num1, m, num2, n) {
     let right = left + gap;
     while (right < length) {
       // left in first array and right in second array
-      if (left < n && right > m) {
-        swapThevalues(array1,array2,left , right - m)
+      if (left < m && right >= n) {
+        swapThevalues(num1, num2, left, right - m);
       }
       // both are in the second array
-      else if (left > m) {
-        swapThevalues(array1,array2,left-n,right-n )
+      else if (left >= m) {
+        swapThevalues(num1, num2, left - m, right - m);
+
       } // both are in the first array
       else {
-        swapThevalues(arr)
+        swapThevalues(num1, num2, left, right-m);
+
       }
+      left++, right++;
     }
+
+    if (gap === 1) break;
+    gap = Math.ceil(gap / 2);
   }
+  num1 = num1.slice(0,m)
+  console.log(num1)
+
+  return [...num1, ...num2];
 }
 
 function swapThevalues(array1, array2, ind1, ind2) {
-  return ([array1[ind1], array2[ind2]] = [array2[ind1], array1[ind2]]);
+  if (array1[ind1] > array2[ind2]) {
+    [array1[ind1], array2[ind2]] = [array2[ind2], array1[ind1]];
+  }
 }
+
+
+//console.log(optimal2([1, 2, 3,0,0,0], 3, [2, 5,6], 3));
+
+
+// using two pointer technique
+function optimalSolutionThreeForLeetCode(nums1, m, nums2, n) {
+   let p1 = m -1
+   let p2 = n -1
+   for(let p = m + n -1 ; p >= 0 ; p--){
+      if(p2 < 0){
+        break
+      }
+
+      if(p1 >= 0 && nums1[p1] > nums2[p2]){
+              nums1[p] = nums1[p1]
+              p1--
+      }else{
+         nums1[p] = nums2[p2]
+         p2--
+      }
+   }
+   return nums1
+}
+
+console.log(
+  optimalSolutionThreeForLeetCode([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+);
