030733328479b67f54adf244829db9aff5db6bf2
RstTiwari <iamrrt2121@gmail.com>
2025-01-06 22:45:49 +0530

    the optimal version

--- Revise_LeetCode/set-matrix-zeroes.py
+++ Revise_LeetCode/set-matrix-zeroes.py
@@ -8,9 +8,12 @@ def mark_row(mat:list,m ,n , row ):
 
 
 def mark_col(mat:list,m, n, col):
+    print(m,col)
     for row in range(m):
+        print(row,col)
         if mat[row][col] != 0:
             mat[row][col] = -1
+    print(mat)
 
 
 def brute_force (mat):
@@ -20,8 +23,11 @@ def brute_force (mat):
     for i in range (m):
         for j in range(n):
             if mat[i][j] == 0:
+                print(mat[i][j])
                 mark_row(mat,m ,n ,i)
                 mark_col(mat,m,n,j)
+              
+    
     for i in range(m):
         for j in range(n):
             if mat[i][j] == -1:
@@ -31,20 +37,21 @@ def brute_force (mat):
 
 
 
-print(brute_force(
-    [
-    [0,1,2,0],
-    [3,4,5,2],
-    [1,3,1,5],
-    ]))
+# print(brute_force(
+#     [
+#     [0,1,2,0],
+#     [3,4,5,2],
+#     [1,3,1,5],
+#     ]))
 
 
-print(brute_force([[0,1]]))
-print(brute_force(
-    [[0],
-     [2],
-     [3]]
-     ))
+# print(brute_force([[0,1]]))
+# print(brute_force(
+#     [[0],
+#      [2],
+#      [3]]
+#      ))
+print(brute_force([[-1],[2],[3]]))
 
 
 
