f10906e42862e7ac08fc66686f344c7a367905e9
RtTiwari <iamrrt2121@gmail.com>
2025-02-06 20:52:21 +0530

    kande algorithm

--- Oprating_system/Lec15.py
+++ Oprating_system/Lec15.py
@@ -1,8 +1,8 @@
 # Concurrency
 
-'''Concurrency refers to the ability of an operating system to handle multiple tasks at the same time. However,
+"""Concurrency refers to the ability of an operating system to handle multiple tasks at the same time. However,
 it doesn't necessarily mean tasks are executed at the same time — they can be interleaved, where the CPU switches 
-between tasks quickly to give the illusion of simultaneous execution.'''
+between tasks quickly to give the illusion of simultaneous execution."""
 import asyncio
 
 
@@ -16,21 +16,21 @@ async def con_b_task():
     print("Con Task b started")
     await asyncio.sleep(1)
     print("concurrent task b completed")
-    
 
 
 async def main():
-    await asyncio.gather(con_a_task(),con_b_task())
+    await asyncio.gather(con_a_task(), con_b_task())
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-    
 
 
 # MultiThreading
 import threading
 import time
+
+
 def print_numbers():
     for i in range(5):
         time.sleep(0.1)
@@ -41,7 +41,8 @@ def print_char():
     for s in "abcde":
         time.sleep(0.1)
         print(s)
-        
+
+
 thread1 = threading.Thread(target=print_numbers)
 thread2 = threading.Thread(target=print_char)
 thread1.start()
@@ -49,5 +50,3 @@ thread2.start()
 thread1.join()
 thread1.join()
 print("Main program is not waiting for threads to finish!")
-
-
--- Revise_LeetCode/kande_algorithm.py
+++ Revise_LeetCode/kande_algorithm.py
@@ -1,42 +1,42 @@
 import sys
-def kande_naive(array:list):
+
+
+def kande_naive(array: list):
     max_sum = 0
     n = len(array)
     for i in range(n):
         tem_sum = array[i]
-        for j in range(i+1,n):
+        for j in range(i + 1, n):
             tem_sum = tem_sum + array[j]
             if tem_sum > max_sum:
                 max_sum = tem_sum
-       
+
     return max_sum
+
 
 # Time Com = O(n^2)
 
 
+def kande_better(array: list):
+    max_sum = -sys.maxsize - 1
+    n = len(array)
+    tem_sum = 0
+    for i in range(n):
+        tem_sum =tem_sum + array[i]
+        if tem_sum > max_sum:
+            max_sum = tem_sum
+            
+        if tem_sum < 0:
+            tem_sum = 0
+    return max_sum
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def kande_better_with_array(array):
+    print("parivar")
 
 if __name__ == "__main__":
-    array =  [-2,1,-3,4,-1,2,1,-5,4]
+    array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    array1 = [1]
     sum1 = kande_naive(array)
-    print("kande algorithm for naive approch",sum1)
+    print("kande algorithm for naive approch", sum1)
+    max_sum1 = kande_better(array1)
+    print("kande algorith for better", max_sum1)
