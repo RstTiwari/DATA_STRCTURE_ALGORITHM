8dd1f73e6d3c087d97d9807f0efffcb14521c9ca
RtTiwari <iamrrt2121@gmail.com>
2025-02-07 10:45:09 +0530

    kande algorithm with sub array

--- Revise_LeetCode/kande_algorithm.py
+++ Revise_LeetCode/kande_algorithm.py
@@ -25,13 +25,41 @@ def kande_better(array: list):
         tem_sum =tem_sum + array[i]
         if tem_sum > max_sum:
             max_sum = tem_sum
-            
+
         if tem_sum < 0:
             tem_sum = 0
     return max_sum
 
+
 def kande_better_with_array(array):
-    print("parivar")
+    max_sum = -sys.maxsize
+    n = len(array)
+    tem_sum = 0
+    start = 0
+    ansStart = 0
+    ansEnd = 0
+    for i in range(n):
+        if tem_sum == 0:
+            start = i
+        tem_sum += array[i]
+        if tem_sum > max_sum:
+            max_sum = tem_sum
+            ansStart = start
+            ansEnd = i
+        if tem_sum < 0:
+            tem_sum = 0
+    return array[ansStart:ansEnd+1]
+
+
+def kande_sum_equals_target(array, target):
+    n = len(array)
+    tem_sum = 0
+    for i in range(i):
+        tem_sum += array
+        if tem_sum == target:
+            return True
+    return False
+
 
 if __name__ == "__main__":
     array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
@@ -40,3 +68,5 @@ if __name__ == "__main__":
     print("kande algorithm for naive approch", sum1)
     max_sum1 = kande_better(array1)
     print("kande algorith for better", max_sum1)
+    array = kande_better_with_array(array)
+    print("kande algorith for better", array)
