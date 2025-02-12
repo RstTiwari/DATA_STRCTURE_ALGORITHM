74c488b183f954e9e070e50a5f722c7ada82a599
RtTiwari <iamrrt2121@gmail.com>
2025-02-12 10:59:50 +0530

    counting the number of inversion

--- Revise_LeetCode/number_of_inversion.py
+++ Revise_LeetCode/number_of_inversion.py
@@ -1,14 +1,31 @@
-def merge_sorted_array(left,right,count):
-    while left and right:
-        
-
-def optimal_solution_with_merge(array:list):
-    n = len(array)
-    count = 0
-    mid = n // 2
-    left = array[:mid]
-    right = array[mid:]
-    count  += merge_sorted_array(left,right,count)
+def merge(left, right, count):
+    n = len(left)
+    start = 0
+    while start < n:
+        if left[start] > right[start]:
+            count += n - start
+        else:
+            left.pop(start)
+        start += 1
     return count
-    
-    
+
+
+def merge_sort(array: list, left ,right):
+    cnt = 0
+    if left >= right:
+        return cnt
+    mid = n // 2
+    cnt += merge_sort(array, left ,mid)
+    cnt += merge_sort(array, mid,right)
+    cnt += merge(left, right, count)
+    return count
+
+def number_inversion(array,n):
+    n = len(array)
+    return merge_sort(array,0,n-1)
+
+if __name__ == "__main__":
+    a = [5, 4, 3, 2, 1]
+    n = 5
+    cnt = optimal_solution_with_merge(a, n)
+    print("The number of inversions are:", cnt)
