cb70b3b9020316472a49d437b30e792453da2708
Rst(Rohit) Tiwari <rohittiwari@hkbdevelopment.com>
2025-04-16 22:09:37 +0530

    added Databse questions

--- practise.js
+++
@@ -1,7 +0,0 @@
-
-let array = []
-for (let i = 1 ; i <=10 ; i ++){
-    array.push(i**2)
-}
-
-console.log(array)
--- reactjs/src/Test.tsx
+++ reactjs/src/Test.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import { Button, Row, Col } from 'antd';
+import React from "react";
+import { Button, Row, Col } from "antd";
 
 const Test: React.FC = () => (
-  <Row gutter={[8, 8]} wrap>
-    <Col>
-    </Col>
-    <Col>
-      <Button>Default Button</Button>
-    </Col>
-    <Col>
-      <Button type="dashed">Dashed Button</Button>
-    </Col>
-    <Col>
-      <Button type="text">Text Button</Button>
-    </Col>
-    <Col>
-      <Button type="link">Link Button</Button>
-    </Col>
-  </Row>
+    <Row gutter={[8, 8]} wrap>
+        <Col></Col>
+        <Col>
+            <Button>Default Button</Button>
+        </Col>
+        <Col>
+            <Button type="dashed">Dashed Button</Button>
+        </Col>
+        <Col>
+            <Button type="text">Text Button</Button>
+        </Col>
+        <Col>
+            <Button type="link">Link Button</Button>
+        </Col>
+    </Row>
 );
-
-export default Test
+export default Test;
