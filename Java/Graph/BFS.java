b86817da79ed4326d4d8e990c5755e60f7f18b87
RtTiwari <iamrrt2121@gmail.com>
2025-06-12 01:20:48 +0530

    learn to addd basic graph dfs and BFS

--- Java/Graph/BFS.java
+++ Java/Graph/BFS.java
@@ -1,15 +1,57 @@
 package Graph;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class BFS {
 
-    public LinkedList<Integer>[] bfsGraph(int v, LinkedList<Integer>[] adjList){
-        boolean vis[] = new boolean[v];
-        Queue <Integer> q = new LinkedList<>();
-        
-        
+    private int V;
+    private LinkedList<Integer>[] adjList;
+
+    public BFS(int v) {
+        V = v;
+        adjList = new LinkedList[v];
+
+        for (int i = 0; i < V; i++) {
+            adjList[i] = new LinkedList<>();
+        }
     }
-    
-}  
+
+    public void addEdege(int u, int v) {
+        adjList[u].add(v);
+        adjList[v].add(u);
+    } 
+
+    public List<Integer> bfs(int start) {
+        List<Integer> trasversal = new ArrayList<>();
+        boolean[] visit = new boolean[V];
+        Queue<Integer> q = new LinkedList<>();
+
+        visit[start] = true;
+        q.add(start);
+        while (!q.isEmpty()) {
+            int current = q.poll();
+            trasversal.add(current);
+
+            for (Integer neb : adjList[current]) {
+                if (!visit[neb]) {
+                    q.add(neb);
+                    visit[neb] = true;
+                }
+            }
+        }
+        return trasversal;
+    }
+
+    public static void main(String[] args) {
+        BFS tree = new BFS(5);
+        tree.addEdege(0, 1);
+        tree.addEdege(0, 2);
+        tree.addEdege(1, 3);
+        tree.addEdege(2, 4);
+        tree.addEdege(3, 4);
+        List<Integer> bfs = tree.bfs(0);
+        System.out.println(bfs);
+
+    }
+
+}
---
+++ Java/Graph/DFS.java
@@ -0,0 +1,68 @@
+package Graph;
+
+import java.util.*;
+
+public class DFS {
+
+    private int V;
+    private LinkedList<Integer>[] adjList;
+
+    public DFS(int v) {
+        V = v;
+        adjList = new LinkedList[v];
+        for (int i = 0; i < V; i++) {
+            adjList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int u, int v) {
+        adjList[u].add(v);
+        adjList[v].add(u);
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < V; i++) {
+            for (int it : adjList[i]) {
+                System.out.println(i + ":" + it);
+            }
+        }
+    }
+
+    public void dfsRecursion(int node, List<Integer> trasversal, boolean[] vis) {
+        vis[node] = true;
+        trasversal.add(node);
+
+        for (Integer it : adjList[node]) {
+            if (!vis[it]) {
+                dfsRecursion(it, trasversal, vis);
+            }
+
+        }
+
+    }
+
+    public List<Integer> dfsGraph(int start) {
+        List<Integer> traseversal = new ArrayList<>();
+        boolean vis[] = new boolean[this.V + 1];
+        vis[start] = true;
+        dfsRecursion(start, traseversal, vis);
+        return traseversal;
+
+    }
+
+    public static void main(String[] args) {
+        DFS g = new DFS(9);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 5);
+        g.addEdge(2, 6);
+        g.addEdge(3, 4);
+        g.addEdge(4, 7);
+        g.addEdge(7, 8);
+        g.addEdge(8, 3);
+        g.printGraph();
+        List<Integer> tra = g.dfsGraph(1);
+        System.out.println(tra);
+
+    }
+}
