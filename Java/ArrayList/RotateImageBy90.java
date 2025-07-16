991885339995d40c19e4cfd5667940a6f2367eec
RtTiwari <iamrrt2121@gmail.com>
2025-07-16 09:24:29 +0530

    added changes and reverse by 90  matrix

--- Java/ArrayList/RotateImageBy90.java
+++ Java/ArrayList/RotateImageBy90.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class RotateImageBy90 {
 
-    public void rotate(int[][] mat) {
+    public void rotateRowBy90(int[][] mat) {
         int[][] transMat = transpose(mat);
         System.out.println(".()" + Arrays.deepToString(transMat) + " " + 3 / 2);
         for (int i = 0; i < transMat.length; i++) {
@@ -31,7 +31,9 @@ public class RotateImageBy90 {
     public static void main(String[] var) {
         int[][] mat = {{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
         RotateImageBy90 rotatedImageClass = new RotateImageBy90();
-        rotatedImageClass.rotate(mat);
+        rotatedImageClass.rotateRowBy90(mat);
+        // if you want to rotate the matrix by 180 degree rotate it twice in positions
+        rotatedImageClass.rotateRowBy90(mat);
         String string = Arrays.deepToString(mat);
         System.out.println(".()" + string);
     }
