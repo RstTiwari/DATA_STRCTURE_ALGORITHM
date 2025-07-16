d21735128461aa80d94b8730885dc754be14b87c
RtTiwari <iamrrt2121@gmail.com>
2025-07-16 09:18:29 +0530

    added changes and reverse se matrix

---
+++ Java/ArrayList/RotateImageBy90.java
@@ -0,0 +1,38 @@
+package ArrayList;
+
+import java.util.Arrays;
+
+public class RotateImageBy90 {
+
+    public void rotate(int[][] mat) {
+        int[][] transMat = transpose(mat);
+        System.out.println(".()" + Arrays.deepToString(transMat) + " " + 3 / 2);
+        for (int i = 0; i < transMat.length; i++) {
+            for (int j = 0; j < transMat.length / 2; j++) {
+                int tem = transMat[i][j];
+                transMat[i][j] = transMat[i][transMat.length - 1 - j];
+                transMat[i][transMat.length - 1 - j] = tem;
+            }
+        }
+    }
+
+    public int[][] transpose(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = i; j < mat[0].length; j++) {
+                int tem = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = tem;
+            }
+        }
+
+        return mat;
+    }
+
+    public static void main(String[] var) {
+        int[][] mat = {{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
+        RotateImageBy90 rotatedImageClass = new RotateImageBy90();
+        rotatedImageClass.rotate(mat);
+        String string = Arrays.deepToString(mat);
+        System.out.println(".()" + string);
+    }
+}
